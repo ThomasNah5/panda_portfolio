6d34cdb0c1c9f00dc4d917fd7e3b39d4d4647b1d
ThomasNah5 <nahthomas0@gmail.com>
2026-07-24 17:23:18 +0000

    new commit

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-xl font-light text-wrap p-8 mt-2 w-96 hidden md:block">About</div>
     <div class="flex flex-row md:flex-row flex-col items-center">
-        <h1 class="text-xl font-light text-wrap pl-8 pr-8 mt-2 w-full md:w-96 text-center md:text-left">I’m Panda, an art director based in Monrovia, Liberia. 
+        <h1 class="text-xl font-light text-wrap pl-8 pr-8 mt-12 w-full md:w-96 text-center md:text-left">I’m Panda, an art director based in Monrovia, Liberia. 
             I create art to satisfy my curious mind. Rather than limiting myself to a single medium, 
             I work with whatever materials or forms I can get my hands on in the moment.
             People call the things I make art, but I prefer to think of them as my little experiments, 
