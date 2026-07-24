3918eb3fb9f1a26ef1b4320c8948dbbb53c7fd64
ThomasNah5 <nahthomas0@gmail.com>
2026-07-24 17:21:52 +0000

    adding text center

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="text-xl font-light text-wrap p-8 mt-2 w-96">About</div>
-    <div class="flex flex-row">
-        <h1 class="text-xl font-light text-wrap pl-8 pr-8 mt-2 w-96">I’m Panda, an art director based in Monrovia, Liberia. 
+    <div class="text-xl font-light text-wrap p-8 mt-2 w-96 hidden md:block">About</div>
+    <div class="flex flex-row md:flex-row flex-col items-center">
+        <h1 class="text-xl font-light text-wrap pl-8 pr-8 mt-2 w-full md:w-96 text-center md:text-left">I’m Panda, an art director based in Monrovia, Liberia. 
             I create art to satisfy my curious mind. Rather than limiting myself to a single medium, 
             I work with whatever materials or forms I can get my hands on in the moment.
             People call the things I make art, but I prefer to think of them as my little experiments, 
             each one an opportunity to explore, question, and discover something new.
         </h1>
-        <img src="/images/home.jpeg" alt="About" class="w-96 h-96 object-cover mt-4">
+        <img src="/images/home.jpeg" alt="About" class="w-96 h-96 object-cover mt-4 hidden md:block">
     </div>
 </template>
   
