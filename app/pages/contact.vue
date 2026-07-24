093d60153bdbc918964355f3f286d0ff0c43ae59
ThomasNah5 <nahthomas0@gmail.com>
2026-07-24 17:42:49 +0000

    new

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,4 +1,7 @@
 <template>
-    <h1>Contact page</h1>
+    <div class="flex flex-col items-center justify-center h-screen overflow-hidden">
+        <h1 class="text-center text-5xl font-light">Let's chat</h1>
+        <p class="text-center text-lg font-light"><a href="mailto:uglyfaces404@gmail.com">uglyfaces404@gmail.com</a> | <a href="tel:+231771911923">+231771911923</a></p>
+    </div>
 </template>
   
